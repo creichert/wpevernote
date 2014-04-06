a749428280c815f49cd5e8b9c82739035dbf36de
Christopher Reichert <creichert07@gmail.com>
2014-04-06 10:07:01 -0500

    README.md in Wordpress plugin format.
    Generate readme.txt from README.md in deploy zip.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp README.md readme.txt
 zip -9 -r --exclude=*.git* --exclude=*~ \
      --exclude=*.swp --exclude=README.md \
      --exclude=deploy.sh  wpevernote.zip .
+rm readme.txt
