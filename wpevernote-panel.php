b9be31181571032781dd1babad6adca441a2fc54
Christopher Reichert <creichert07@gmail.com>
2014-04-05 20:17:35 -0500

    Add GPL license.

--- wpevernote-panel.php
+++ wpevernote-panel.php
@@ -1,3 +1,26 @@
+<!--
+    Copyright 2014  Christopher Reichert <christopher@reichertbrothers.com>
+	The code used in WP Evernote is based on:
+        Plugin Name: EverPress
+        Plugin URI: http://mashe.hawksey.info/everpress-plugin/
+        Description: EverPress allows Evernote users to automatic post their shared notebooks to WordPress.
+        Author: Martin Hawksey
+        Author URI: http://mashe.hawksey.info
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+-->
 <?php if (isset($_POST['wpevernote_action'])) { ?>
 <div id="message" class="updated fade" style="background-color: rgb(255, 251, 204);">
   <p><strong><?php echo $status; ?></strong></p>
--- wpevernote.php
+++ wpevernote.php
@@ -2,13 +2,37 @@
 
 /*
 Plugin Name: WP Evernote
-Plugin URI:
+Plugin URI: https://github.com/creichert/wpevernote
 Description: WP Evernote allows Evernote users to automatically post
              public notebooks to WordPress.
 Author: Christopher Reichert
 Author URI: http://reichertbrothers.com
 Version: 0.1
 */
+
+/*  Copyright 2014  Christopher Reichert <christopher@reichertbrothers.com>
+	The code used in WP Evernote is based on:
+        Plugin Name: EverPress
+        Plugin URI: http://mashe.hawksey.info/everpress-plugin/
+        Description: EverPress allows Evernote users to automatic post their shared notebooks to WordPress.
+        Author: Martin Hawksey
+        Author URI: http://mashe.hawksey.info
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . dirname(__FILE__) . "/evernote-sdk-php/lib" . PATH_SEPARATOR);
 
